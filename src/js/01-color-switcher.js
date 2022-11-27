f660b57ebadeb89b1c285990ea3020d41667ffe5
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-11-27 20:54:20 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyForColors = document.querySelector(`body`);
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
+let timerID = null;
 
+btnStart.addEventListener(`click`, noChengesColorBody);
+btnStop.addEventListener(`click`, offChengesColorBody);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function noChengesColorBody() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerID = setInterval(() => {
+    bodyForColors.style.backgroundColor = getRandomHexColor();
+    console.log(`color`);
+  }, 1000);
+}
+
+function offChengesColorBody() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerID);
+}
